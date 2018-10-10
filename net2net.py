da88d9f9a2167e120981e1eb6fa7e9d69d89b3dd
Calvin Ku <calvin.j.ku@googlemail.com>
2018-10-10 14:46:27 +0800

    refactored until line 146

--- net2net.py
+++ net2net.py
@@ -12,7 +12,7 @@ def wider(m1, m2, new_width, bnorm=None, out_size=None, noise=True,
         m1 - module to be wider
         m2 - follwing module to be adapted to m1
         new_width - new width for m1.
-        bn (optional) - batch norm layer, if there is btw m1 and m2
+        bn (optional) - batch norm layer, if there is one btw m1 and m2
         out_size (list, optional) - necessary for m1 == conv3d and m2 == linear. It
             is 3rd dim size of the output feature map of m1. Used to compute
             the matching Linear layer size
@@ -27,61 +27,102 @@ def wider(m1, m2, new_width, bnorm=None, out_size=None, noise=True,
     w2 = m2.weight.data
     b1 = m1.bias.data
 
+    w1_input_num_units = w1.size(1)
+    w1_num_kernels = w1.size(0) 
+    w2_input_num_units = w2.size(1)
+    w2_output_num_units = w2.size(0)
+
+    # Check if the new layer is wider:
+    if new_width <= w1_num_kernels:
+        raise ValueError("New size should be larger.")
+
     if "Conv" in m1.__class__.__name__ or "Linear" in m1.__class__.__name__:
         # Convert Linear layers to Conv if linear layer follows target layer
         if "Conv" in m1.__class__.__name__ and "Linear" in m2.__class__.__name__:
-            assert w2.size(1) % w1.size(0) == 0, "Linear units need to be multiple"
+            # Check if the input shape (length) of the linear layer, is a multiple of the #kernels
+            # of the conv layer 
+            # (because the shape of the flatten tensor must be the multiple of the #kernels)
+            if w2_input_num_units % w1_num_kernels != 0:
+                raise ValueError("Number of linear units need to be multiple of number of kernels in conv layer")
+            
+            # Check tensor rank
             if w1.dim() == 4:
-                factor = int(np.sqrt(w2.size(1) // w1.size(0)))
-                w2 = w2.view(w2.size(0), w2.size(1)//factor**2, factor, factor)
-            elif w1.dim() == 5:
+                feature_map_size = w2_input_num_units // w1_num_kernels
+                factor = int(np.sqrt(feature_map_size))
+                # Convert linear layer to conv layer
+                w2 = w2.view(w2_output_num_units, w2_input_num_units // feature_map_size, factor, factor)
+            elif w1.dim() == 5: #TODO: to be addressed later
                 assert out_size is not None,\
                        "For conv3d -> linear out_size is necessary"
                 factor = out_size[0] * out_size[1] * out_size[2]
                 w2 = w2.view(w2.size(0), w2.size(1)//factor, out_size[0],
                              out_size[1], out_size[2])
-        else:
-            assert w1.size(0) == w2.size(1), "Module weights are not compatible"
-        assert new_width > w1.size(0), "New size should be larger"
+            else:
+                raise ValueError("Data shape not supported: {}".format(w1.dim()))
 
-        old_width = w1.size(0)
-        nw1 = m1.weight.data.clone()
-        nw2 = w2.clone()
+        else: # For cases where m1 and m2 are both linear layers or both conv layers
+            if w1_num_kernels != w2_input_num_units:
+                raise ValueError("Module weights are not compatible.")
 
-        if nw1.dim() == 4:
-            nw1.resize_(new_width, nw1.size(1), nw1.size(2), nw1.size(3))
-            nw2.resize_(nw2.size(0), new_width, nw2.size(2), nw2.size(3))
-        elif nw1.dim() == 5:
-            nw1.resize_(new_width, nw1.size(1), nw1.size(2), nw1.size(3), nw1.size(4))
-            nw2.resize_(nw2.size(0), new_width, nw2.size(2), nw2.size(3), nw2.size(4))
-        else:
-            nw1.resize_(new_width, nw1.size(1))
-            nw2.resize_(nw2.size(0), new_width)
+        ############################################################ 
+        # Now m1 and m2 are either both linear layers or conv layers
+        ############################################################
 
+        old_width = w1_num_kernels
+        w1_widen = w1.clone()
+        w2_widen = w2.clone()
+
+        # Widen m1 with placeholder values and m2 accordingly (while both are conv layers)
+        if w1_widen.dim() == 4: 
+            w1_widen.resize_(new_width, w1_widen.size(1), w1_widen.size(2), w1_widen.size(3))
+            w2_widen.resize_(w2_widen.size(0), new_width, w2_widen.size(2), w2_widen.size(3))
+        elif w1_widen.dim() == 5: #TODO: to be addressed later
+            w1_widen.resize_(new_width, w1_widen.size(1), w1_widen.size(2), w1_widen.size(3), w1_widen.size(4))
+            w2_widen.resize_(w2_widen.size(0), new_width, w2_widen.size(2), w2_widen.size(3), w2_widen.size(4))
+        # Widen m1 with placeholder values and m2 accordingly (while both are linear layers)
+        else: 
+            w1_widen.resize_(new_width, w1_widen.size(1))
+            w2_widen.resize_(w2_widen.size(0), new_width)
+
+        # Widen bias term of m1
         if b1 is not None:
-            nb1 = m1.bias.data.clone()
-            nb1.resize_(new_width)
+            b1_widen = m1.bias.data.clone()
+            b1_widen.resize_(new_width) # NB: can't add bias to feature maps thru braodcasting directly in PyTorch
 
         if bnorm is not None:
-            nrunning_mean = bnorm.running_mean.clone().resize_(new_width)
-            nrunning_var = bnorm.running_var.clone().resize_(new_width)
-            if bnorm.affine:
-                nweight = bnorm.weight.data.clone().resize_(new_width)
-                nbias = bnorm.bias.data.clone().resize_(new_width)
+            running_mean_widen = bnorm.running_mean.clone().resize_(new_width)
+            running_var_widen = bnorm.running_var.clone().resize_(new_width)
 
+            if bnorm.affine:
+                weight_widen = bnorm.weight.data.clone().resize_(new_width)
+                bias_widen = bnorm.bias.data.clone().resize_(new_width)
+
+        # Switch input / output position in m2 weight tensor
         w2 = w2.transpose(0, 1)
-        nw2 = nw2.transpose(0, 1)
+        w2_widen = w2_widen.transpose(0, 1) # NB: w2_widen is already widen and is larger w2
 
-        nw1.narrow(0, 0, old_width).copy_(w1)
-        nw2.narrow(0, 0, old_width).copy_(w2)
-        nb1.narrow(0, 0, old_width).copy_(b1)
+        # Copy teacher weights onto the corresponding pos. in student's
+        print("Weight dims:")
+        print("W1 (widen) weight shape: {}".format(w1_widen.size()))
+        print("W2 (widen) weight shape: {}".format(w2_widen.size()))
+        print("B1 (widen) weight shape: {}".format(b1_widen.size()))
+        print("")
+        w1_widen.narrow(0, 0, old_width).copy_(w1)
+        w2_widen.narrow(0, 0, old_width).copy_(w2)
+        b1_widen.narrow(0, 0, old_width).copy_(b1)
 
+        # Copy teacher batchnorm params onto the corresponding pos. in student's
         if bnorm is not None:
-            nrunning_var.narrow(0, 0, old_width).copy_(bnorm.running_var)
-            nrunning_mean.narrow(0, 0, old_width).copy_(bnorm.running_mean)
+            # running_var_widen.narrow(0, 0, old_width).copy_(bnorm.running_var)
+            # running_mean_widen.narrow(0, 0, old_width).copy_(bnorm.running_mean)
+            running_mean_widen[0:old_width].copy_(bnorm.running_mean)
+            running_var_widen[0:old_width].copy_(bnorm.running_var)
+
             if bnorm.affine:
-                nweight.narrow(0, 0, old_width).copy_(bnorm.weight.data)
-                nbias.narrow(0, 0, old_width).copy_(bnorm.bias.data)
+                # weight_widen.narrow(0, 0, old_width).copy_(bnorm.weight.data)
+                # bias_widen.narrow(0, 0, old_width).copy_(bnorm.bias.data)
+                weight_widen[0:old_width].copy_(bnorm.weight.data)
+                bias_widen[0:old_width].copy_(bnorm.bias.data)
 
         # TEST:normalize weights
         if weight_norm:
@@ -89,76 +130,78 @@ def wider(m1, m2, new_width, bnorm=None, out_size=None, noise=True,
                 norm = w1.select(0, i).norm()
                 w1.select(0, i).div_(norm)
 
-        # select weights randomly
+        # Select weights randomly
         tracking = dict()
         for i in range(old_width, new_width):
+            # Pick from the old weights randomly
             idx = np.random.randint(0, old_width)
+            # Record the selected weight positions
             try:
                 tracking[idx].append(i)
             except:
                 tracking[idx] = [idx]
                 tracking[idx].append(i)
 
-            # TEST:random init for new units
+            # TEST: random init for new units
             if random_init:
                 n = m1.kernel_size[0] * m1.kernel_size[1] * m1.out_channels
                 if m2.weight.dim() == 4:
                     n2 = m2.kernel_size[0] * m2.kernel_size[1] * m2.out_channels
-                elif m2.weight.dim() == 5:
+                elif m2.weight.dim() == 5: #TODO: To be addressed later
                     n2 = m2.kernel_size[0] * m2.kernel_size[1] * m2.kernel_size[2] * m2.out_channels
                 elif m2.weight.dim() == 2:
                     n2 = m2.out_features * m2.in_features
-                nw1.select(0, i).normal_(0, np.sqrt(2./n))
-                nw2.select(0, i).normal_(0, np.sqrt(2./n2))
+                w1_widen.select(0, i).normal_(0, np.sqrt(2./n))
+                w2_widen.select(0, i).normal_(0, np.sqrt(2./n2))
             else:
-                nw1.select(0, i).copy_(w1.select(0, idx).clone())
-                nw2.select(0, i).copy_(w2.select(0, idx).clone())
-            nb1[i] = b1[idx]
+                w1_widen.select(0, i).copy_(w1.select(0, idx).clone())
+                w2_widen.select(0, i).copy_(w2.select(0, idx).clone())
+            b1_widen[i] = b1[idx]
 
         if bnorm is not None:
-            nrunning_mean[i] = bnorm.running_mean[idx]
-            nrunning_var[i] = bnorm.running_var[idx]
+            running_mean_widen[i] = bnorm.running_mean[idx]
+            running_var_widen[i] = bnorm.running_var[idx]
             if bnorm.affine:
-                nweight[i] = bnorm.weight.data[idx]
-                nbias[i] = bnorm.bias.data[idx]
+                weight_widen[i] = bnorm.weight.data[idx]
+                bias_widen[i] = bnorm.bias.data[idx]
             bnorm.num_features = new_width
 
         if not random_init:
             for idx, d in tracking.items():
                 for item in d:
-                    nw2[item].div_(len(d))
+                    w2_widen[item].div_(len(d))
 
         w2.transpose_(0, 1)
-        nw2.transpose_(0, 1)
+        w2_widen.transpose_(0, 1)
 
         m1.out_channels = new_width
         m2.in_channels = new_width
 
         if noise:
-            noise = np.random.normal(scale=5e-2 * nw1.std(),
-                                     size=list(nw1.size()))
-            nw1 += th.FloatTensor(noise).type_as(nw1)
+            noise = np.random.normal(scale=5e-2 * w1_widen.std(),
+                                     size=list(w1_widen.size()))
+            w1_widen += th.FloatTensor(noise).type_as(w1_widen)
 
-        m1.weight.data = nw1
+        m1.weight.data = w1_widen
 
         if "Conv" in m1.__class__.__name__ and "Linear" in m2.__class__.__name__:
             if w1.dim() == 4:
-                m2.weight.data = nw2.view(m2.weight.size(0), new_width*factor**2)
+                m2.weight.data = w2_widen.view(m2.weight.size(0), new_width*factor**2)
                 m2.in_features = new_width*factor**2
             elif w2.dim() == 5:
-                m2.weight.data = nw2.view(m2.weight.size(0), new_width*factor)
+                m2.weight.data = w2_widen.view(m2.weight.size(0), new_width*factor)
                 m2.in_features = new_width*factor
         else:
-            m2.weight.data = nw2
+            m2.weight.data = w2_widen
 
-        m1.bias.data = nb1
+        m1.bias.data = b1_widen
 
         if bnorm is not None:
-            bnorm.running_var = nrunning_var
-            bnorm.running_mean = nrunning_mean
+            bnorm.running_var = running_var_widen
+            bnorm.running_mean = running_mean_widen
             if bnorm.affine:
-                bnorm.weight.data = nweight
-                bnorm.bias.data = nbias
+                bnorm.weight.data = weight_widen
+                bnorm.bias.data = bias_widen
         return m1, m2, bnorm
 
 
